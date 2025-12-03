ff76b6edafd6580a7e1bad5bcffaa5d80fa75821
Rad0vanZ <radovan.z2023@gmail.com>
2025-12-03 18:36:46 +0100

    Update rewrites in vercel.js for donate page

--- vercel.js
+++ vercel.js
@@ -2,6 +2,8 @@
   "version": 2,
   "rewrites": [
     { "source": "/", "destination": "/public/index.html" },
+    { "source": "/donate", "destination": "/public/donate.html" },
+    { "source": "/donate.html", "destination": "/public/donate.html" },
     { "source": "/style.css", "destination": "/public/style.css" },
     { "source": "/script.js", "destination": "/public/script.js" },
     { "source": "/api/random", "destination": "/api/random.js" }
